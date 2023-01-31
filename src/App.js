0cf1484fec2d78d39bc61702e2c01ea75ce98781
Rifqi Agnia Mubarok <95258391+rifqiagniamubarok@users.noreply.github.com>
2023-01-31 18:55:33 +0700

    udpate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import QrReader from 'react-qr-scanner';
 import './App.css';
 
 function App() {
+  const [delay, setDelay] = useState(100);
+  const [result, setResult] = useState(null);
+
+  const handleError = (err) => {
+    console.error(err);
+  };
+
+  const handleScan = (data) => {
+    setResult(data);
+  };
+  const previewStyle = {
+    height: 240,
+    width: 320,
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QrReader
+        delay={delay}
+        style={previewStyle}
+        onError={handleError}
+        onScan={handleScan}
+      />
+      <div>
+        <p>heloo : </p>
+        <p>{result ? result?.text : 'noo'} </p>
+      </div>
     </div>
   );
 }
